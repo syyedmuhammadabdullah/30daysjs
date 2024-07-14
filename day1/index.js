49574504485c1378fb3f088711b37afa9de6cd5d
Syed Muhammad Abdullah <144997778+syyedmuhammadabdullah@users.noreply.github.com>
2024-07-14 05:33:56 +0500

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,70 @@
+//number
+var number=10;
+console.log("number is ",number);
+//string
+let string="hello";
+console.log("string is ",string);
+//boolean
+const boolean=true;
+console.log("boolean is ",boolean);
+// create variable of different types such as number string boolean array object etc and log them in console
+
+//type of number
+let num=10;
+console.log("type of num is ",typeof num);
+
+//type of string
+let str="hello";
+console.log("type of str is ",typeof str);
+
+//type of boolean
+let bool=true;
+console.log("type of bool is ",typeof bool);
+
+//type of null
+let nul=null;
+console.log("type of nul is ",typeof nul);
+
+//type of undefined
+let und;
+console.log("type of und is ",typeof und);
+
+
+//type of array
+let array=[1,2,3,4,5];
+console.log("type of array is ",typeof array);
+
+//type of object
+let object={
+    name:"sachin",
+    age:25
+}
+console.log( "type of object is ",typeof object);
+
+//create a variable and assign it a value and log it then reassign it a new value and log it again
+//assigning value
+let variable=10;
+console.log(variable);
+//reassigning value
+variable=20;
+console.log(variable);
+
+
+//try reassigning a variable with the same name as the one you created and observe the error
+// do it yourself by creating a variable with the same name as the one you created and try reassigning it with a new value
+
+
+
+
+//create a constant and try to reassign it a value and observe the error
+const constant=10;
+console.log(constant);
+// constant=20;
+// console.log(constant);
+
+//you will get error assignment to a constant variable is not allowed
+//this means you cannot reassign a constant variable with a new value
+
+
+//congrats you have completed the first part of this tutorial
+
